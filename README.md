4211fab4522e2d5fb07647e5e3b3fb29389a2b86
sankarshan <sankarshan7@gmail.com>
2020-06-22 00:06:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# :rocket: Simple Web framework to showcase computer vision work
+
+- This project aims to create a simple general web framework that can be reused to showcase any computer vision work. The original code and idea is borrowed from [this](https://www.youtube.com/watch?v=BUh76-xD5qU) great video by [Abhishek Thakur](https://www.kaggle.com/abhishek) where he created this simple bootstrap based framework to host an image classification task using a deep learning model. I have slightly modified it. 
+
+- Here I have simplified it, and used a dummy simple random prediction model as a baseline just to create the web framework. The idea is, this project can be reused for other actual computer vision project to showcase your work.  
+
+- Feel free to use it.
+
+----
---
+++ api.py
@@ -0,0 +1,61 @@
+# way to upload image
+
+# way to save the image
+
+# function to make prediction on the image
+
+# show the result
+import os
+from flask import Flask
+from flask import request
+from flask import render_template
+import numpy as np
+import random
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "/home/sankarshan/Documents/code/web_framework_for_data_science/static"
+
+# GET: accessing the website
+# POST: accessing the form
+@app.route("/", methods = ["GET", "POST"])
+def upload_predict():
+    # this function should show index.html
+    # simulate the model prediction value
+    
+    if request.method == "POST":
+        image_file = request.files["image"]
+        if image_file:
+            image_location = os.path.join(
+                UPLOAD_FOLDER, 
+                image_file.filename
+            )
+            image_file.save(image_location)
+    
+            pred_class, pred_proba = get_prediction()
+            return render_template("index.html", 
+                                prediction_class = pred_class, 
+                                prediction_proba = pred_proba,
+                                image_file = image_file.filename)
+
+    return render_template("index.html", 
+                    prediction_class = "no image given", 
+                    prediction_proba = 0, image_file=None)
+
+def get_prediction():
+    """This is a random prediction working as a baseline 
+
+    Returns:
+        Prediction class and probability
+    """
+    
+    ################################################
+    # put your original prediction code here
+    ################################################
+    
+    pred_proba = np.round(np.random.rand(), 4)
+    pred_class = random.choice(["cat", "dog",  "monkey"])
+    return pred_class, pred_proba
+
+if __name__ == "__main__":
+    app.run(port=12000, debug=True)
